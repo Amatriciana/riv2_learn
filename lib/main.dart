09f805b3a13f299a7421bece511f5378539d6913
Amatriciana <amatriciana913@gmail.com>
2022-04-29 19:31:14 +0900

    作業途中

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:path/path.dart';
 import 'package:riv2_learn/bmi_history.dart';
 
 import 'package:riv2_learn/controller.dart';
+import 'package:sqflite/sqflite.dart';
 import 'counter.dart';
 import 'bmi_calculator.dart';
 import 'bmi_history.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+final databaseProvider = Provider<Future<Database>>(
+  (ref) => throw UnimplementedError(),
+);
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  late final Future<Database> database;
+  await Future.wait(
+    [
+      Future(() async {
+        //TODO この処理間違ってる気がする
+        database = openDatabase(
+          join(await getDatabasesPath(), 'bmi_database.db'),
+          onCreate: (db, version) {
+            return db.execute(
+                "CREATE TABLE bmi_history(id INTEGER PRIMARY KEY AUTOINCREMENT, result REAL, height REAL, weight REAL)");
+          },
+          version: 1,
+        );
+      })
+    ],
+  );
+
+  runApp(
+    ProviderScope(
+      overrides: [databaseProvider.overrideWithValue(database)],
+      child: Consumer(
+        builder: (context, ref, child) {
+          return child!;
+        },
+        child: const MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends HookConsumerWidget {
