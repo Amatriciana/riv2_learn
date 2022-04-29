66bd720a8760544590d94df3e2250d6ff28141eb
Amatriciana <amatriciana913@gmail.com>
2022-04-29 12:16:15 +0900

    first

---
+++ android/app/src/main/kotlin/com/example/riv2_learn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riv2_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('title'),
+        ),
+      ),
+    );
+  }
+}
